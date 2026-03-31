1d3f9dd9c04b1fcecc29b85fb86e024e814b17ac
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-31 14:55:32 +0300

    Solution

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -9,12 +9,13 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **kwargs):
-        self.stdout.write('Waiting for database...')
-        db_conn = None
-        while not db_conn:
+        self.stdout.write("Waiting for database...")
+        while True:
             try:
-                db_conn = connections['default']
+                db = connections["default"]
+                db.ensure_connection()
+                break
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available'))
+        self.stdout.write(self.style.SUCCESS("Database available"))
